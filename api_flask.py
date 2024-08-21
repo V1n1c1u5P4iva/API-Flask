44035821dd93f1a5ffe32df0e5bddc948cb232da
Vinicius Paiva <144559632+V1n1c1u5P4iva@users.noreply.github.com>
2024-08-21 15:07:34 -0300

    Update api_flask.py

--- api_flask.py
+++ api_flask.py
@@ -1,10 +1,19 @@
+import os
 import pandas as pd
 from flask import jsonify, Flask
 from pyngrok import ngrok
 import random as r
 
-# Substitua 'YOUR_AUTHTOKEN' pelo seu token de autenticação ngrok
-ngrok.set_auth_token('YOUR_AUTHTOKEN')
+# Obtenha o token de autenticação do ngrok e a porta do ambiente
+ngrok_auth_token = os.getenv('NGROK_AUTH_TOKEN')
+excel_file_path = os.getenv('EXCEL_FILE_PATH')
+ngrok_port = os.getenv('NGROK_PORT', 5000)  # Porta padrão é 5000 se não for especificada
+
+if not ngrok_auth_token or not excel_file_path:
+    raise ValueError("As variáveis de ambiente NGROK_AUTH_TOKEN e EXCEL_FILE_PATH devem ser definidas.")
+
+# Configura o token de autenticação do ngrok
+ngrok.set_auth_token(ngrok_auth_token)
 
 app = Flask(__name__)
 
@@ -15,7 +24,7 @@ def home():
 @app.route("/input")
 def input():
     # Lê a planilha Excel
-    df = pd.read_excel('Digite o caminho')
+    df = pd.read_excel(excel_file_path)
     
     # Converte o DataFrame para JSON
     json_data = df.to_json(orient='records', indent=4)
@@ -36,6 +45,6 @@ def predJson():
 
 if __name__ == '__main__':
     # Inicia o túnel ngrok
-    public_url = ngrok.connect(5000)
+    public_url = ngrok.connect(ngrok_port)
     print(f" * ngrok URL: {public_url}")
-    app.run()
+    app.run(port=ngrok_port)
